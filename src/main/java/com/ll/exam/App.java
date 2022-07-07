66094c8e4731aa4df34b6e47e9220b0b408561c7
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-07-07 17:50:27 +0900

    기능(수정, 목록) 추가

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -3,6 +3,7 @@ package com.ll.exam;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -10,43 +11,44 @@ import org.json.simple.parser.ParseException;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class App {
     ArrayList<Post> list = new ArrayList<>();
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String path = "D:\\CodeLion_Study\\ssg_practice\\src\\main\\java\\com\\ll\\exam\\json\\post.json";
+    long num = 1;
 
+    public App(){
+        jsonRead();
+    }
     public void jsonRead(){
         // FileReader 생성
         Reader reader = null;
         try {
-            reader = new FileReader("D:\\CodeLion_Study\\ssg_practice\\src\\main\\java\\com\\ll\\exam\\json\\post.json");
+            reader = new FileReader(path);
             Gson gson = new Gson();
             JSONArray jsonArray = gson.fromJson(reader, JSONArray.class);
-            // JsonObject 객체 출력
-            // {"id":1,"students":[{"id":123,"name":"Tom"},{"id":124,"name":"Jerry"}],"subject":{"name":"Java","professor":"Anna"}}
-//            Object[] objects = obj.toArray();
-//            for (Object object : objects) {
-//                System.out.println(object.toString());
-//            }
 
+            // Json 파일 읽어서, <Post> List로 변환
             if(jsonArray != null){
-                for(int i = 0; i <jsonArray.size(); i++)
-                    list.add(jsonArray.get(i));
-            }
+                list = new Gson().fromJson(jsonArray.toString(), new TypeToken<List<Post>>() {
+                }.getType());
 
-            for (Post post : list) {
-                System.out.println(post.toString());
+                num = list.get(list.size()-1).num; // 최신번호
+                num += 1L; // 다음번호
+
             }
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-        // Json 파일 읽어서, Lecture 객체로 변환
-
     }
-    public void jsonWrite(){
-        //            Reader reader = new FileReader("db.json");
 
-        JSONObject obj = new JSONObject();
+    public void jsonWrite(){
+
         JSONArray jArray = new JSONArray();
         for(int i = 0; i < list.size(); i++){
             JSONObject sObject = new JSONObject(); // 배열 내에 들어갈 json
@@ -55,35 +57,61 @@ public class App {
             sObject.put("talk", list.get(i).talk);
             jArray.add(sObject);
         }
-
-        obj.put("Name", "Test");
-        obj.put("id", "userId");
-        obj.put("item", jArray);
-
-        System.out.println("test");
-        System.out.println(obj.toString());
-        System.out.println(obj.toJSONString());
-
-
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
+
         try {
-            FileWriter fw = new FileWriter("D:\\CodeLion_Study\\ssg_practice\\src\\main\\java\\com\\ll\\exam\\json\\post.json");
+            FileWriter fw = new FileWriter(path);
             gson.toJson(jArray,fw);
             fw.flush();
             fw.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-
     }
-    public void run() {
-        jsonRead();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("=== 명원 SSG ===");
 
-        Long num = 1L;
+    public void updatePost(int id){
+        Post findPost = findById(id);
+        if(findPost != null){
+            System.out.println(id + "번 명언을 수정합니다.");
+            System.out.println("기존 명언 : " + findPost.talk);
+            System.out.print("새 명언 : ");
+
+            try {
+                String updateTalk = br.readLine();
+                findPost.talk = updateTalk;
+
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println(id + "번 명언이 수정되었습니다.");
+
+        }
+    }
+    public Post findById(int id){
+        for(int i = 0; i < list.size(); i++){
+            if(list.get(i).num == id){
+                return list.get(i);
+            }
+        }
+        System.out.println(id+"번 명언은 존재하지 않습니다.");
+        return null;
+    }
+
+    public void printList(){
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+        for(int i = 0; i < list.size(); i++){
+            System.out.printf("%d / %s / %s\n", list.get(i).num, list.get(i).author, list.get(i).talk);
+        }
+    }
+
+    public void run() {
+
+
+        System.out.println("=== 명원 SSG ===");
 
         outer:
         while(true){
@@ -104,8 +132,18 @@ public class App {
                         System.out.println((num++)+"번이 등록되었습니다.");
                         break;
                     case "수정":
-
-
+                        System.out.print("수정 ID : ");
+                        int id = Integer.parseInt(br.readLine());
+                        updatePost(id);
+                        break;
+                    case "목록":
+                        printList();
+                        break;
+                    case "삭제":
+                        break;
+                    default:
+                        System.out.println("올바르게 입력해주세요.");
+                        break;
 
 
                 }
@@ -121,4 +159,6 @@ public class App {
         }
 
     }
+
+
 }
