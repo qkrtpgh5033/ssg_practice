232dff5d91e1962250516b475c9cb0a5f4c0d234
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-07-07 14:48:31 +0900

    run 메소드(등록) 구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -9,6 +9,8 @@ public class App {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("=== 명원 SSG ===");
 
+        int num = 1;
+
         outer:
         while(true){
             System.out.print("명령) ");
@@ -18,6 +20,13 @@ public class App {
                 switch (cmd){
                     case "종료":
                         break outer;
+                    case "등록":
+                        System.out.print("명언 : ");
+                        String talk = br.readLine();
+                        System.out.print("작가 : ");
+                        String author = br.readLine();
+                        System.out.println((num++)+"번이 등록되었습니다.");
+
                 }
 
             }catch (IOException e){
